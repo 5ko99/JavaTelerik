d64a9d355ffa87e845656bf51190e1537bda9b88
AOC <petko.krasimirov99@gmail.com>
2015-12-31 12:21:47 +0200

    Added more text and examples of Java meeting 3

---
+++ Java/StructuresformData/src/heshTables/heshsets.java
@@ -0,0 +1,12 @@
+package heshTables;
+
+import java.util.*;
+
+public class heshsets {
+	public static void main(String[] args){
+		Set<String> set = new HashSet<>();
+		set.add("Pesho");
+		set.add("Gosho");
+		System.out.println(set);
+	}
+}

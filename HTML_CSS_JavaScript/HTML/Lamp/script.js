5e9c07eff3dcbc422d2ec471e8566d1511e1d903
AOC <petko.krasimirov99@gmail.com>
2016-02-07 15:51:21 +0200

    Added some very cool examples for Ajax and Jqery text and web page that take your location and pirnt your current weather

--- HTML_CSS_JavaScript/HTML/Lamp/script.js
+++ HTML_CSS_JavaScript/HTML/Lamp/script.js
@@ -27,10 +27,32 @@ $('#add-btn').on('click',function(ev){
 	$special.removeClass();
 	var $s = $('.special');
 	printMessage($s.text());
-    
 	});
 
 function printMessage(text){
 	$('#m-list').first().append(text+' ');
 }
 
+    var position = {};
+    navigator.geolocation.getCurrentPosition(function (data){
+    position.longitude= data.coords.longitude;
+    position.latitude = data.coords.latitude;
+    
+    $.ajax({
+        url: 'http://api.openweathermap.org/data/2.5/weather?lat=' +position.latitude +'&lon=' +position.longitude +'&appid=44db6a862fba0b067b1930da0d769e98',
+        method : 'GET',
+        success: function(data){
+            console.log(data);
+            var temp = data.main.temp;
+            temp = temp - 273.15;
+            temp = temp.toFixed(1);
+            var city = data.name;
+            $('#temp').text('Tempereture in '+city+' is: '+temp+'°C');
+        },
+        error: function(er){
+            console.log(er);
+            $('#temp').text('Error with current tempereture.');
+        }
+    });
+
+});
